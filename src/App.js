7e20cf07fb704c61bee453078ddfccc584fae314
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-23 21:24:04 -0300

    Criar a estrutura basica do componente App.js e criar um mock handlerSubmit para o elemento formulario.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 // https://www.youtube.com/watch?v=ly3m6mv5qvg
 // 2 hr 54' 00 ''
 import React, { useState } from 'react';
+
 import data from './data';
+
+
+
 function App() {
+  const [ count, setCount ] = useState(0);
+  const [ text, setText ] = useState([]);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Submiting form...');
+  };
   
-  
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-ipsum" onSubmit={handleSubmit} >
+        <label htmlFor="amout">
+          paragraphs:
+        </label>
+        <input type="number" name="amout" id="amount" />
+      </form>
+    </section>
+    );
 }
 
+
+
 export default App;
+
+
