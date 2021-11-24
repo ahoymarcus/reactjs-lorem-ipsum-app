4a78a7d26eda727e8b8bd0a63b783b04af9ba29d
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-23 21:14:25 -0300

    Iniciar repositorio local

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// https://www.youtube.com/watch?v=ly3m6mv5qvg
+// 2 hr 54' 00 ''
+import React, { useState } from 'react';
+import data from './data';
+function App() {
+  
+  
+  return (
+  <h2>lorem ipsum project setup</h2>
+    )
+}
+
+export default App;
